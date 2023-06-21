6eb88d68997dc4a92443d242aa6f0bc658be3603
OheneAkuffo <ohenekakuffo@gmail.com>
2023-06-20 18:38:35 -0700

    In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a,int b,int c);
+void print_remaining_days(int month,int day,int year);
+int convert_day(int month,int day);
+
+#endif
