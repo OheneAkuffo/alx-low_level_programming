a764c89b9d0c8bea76227e0eac0b911b764112cc
OheneAkuffo <ohenekakuffo@gmail.com>
2023-06-20 17:00:31 -0700

    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * print_times_table - print out timetable
  * @n: number of times to be printed
- * Description:a function that prints the n times table, starting with 0
+ *
+ * Description: A function that prints the n times table, starting with 0.
  */
-
 void print_times_table(int n)
 {
 	int i, j;
 
-	if (!(n > 15 || n < 0))
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -23,10 +24,11 @@ void print_times_table(int n)
 
 /**
  * putformat - format output
- * @n: input paremeter
- * Return: nothing
+ * @n: input parameter
+ *
+ * Description: Formats and outputs the given number.
  */
-void putformat(int n);
+void putformat(int n)
 {
 	if (n <= 9)
 	{
