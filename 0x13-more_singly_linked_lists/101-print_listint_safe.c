eba80f69dcd14719d2e093f3b8938b8b9d10b63e
OheneAkuffo <Ohenekakuffo@gmail.com>
2023-08-01 18:36:59 -0700

    Write a function that prints a listint_t linked list updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 #include "stdio.h"
+
 /**
- * print_listint_safe - prints a linked list, safely in the project
+ * print_listint_safe - prints a linked list safely in the project
  * @head: list of type listint_t to print in the project
- * Return: number of nodes in the list in the project
+ * Return: number of nodes in the list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
